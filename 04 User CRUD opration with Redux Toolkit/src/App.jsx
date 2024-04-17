e00b054e8f0a2bba897f90f4d75f3688b5171927
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-04-17 10:51:57 +0530

    commit

--- 04 User CRUD opration with Redux Toolkit/src/App.jsx
+++ 04 User CRUD opration with Redux Toolkit/src/App.jsx
@@ -1,11 +1,16 @@
 import From from "./Components/From/From";
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <From />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<From />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- 04 User CRUD opration with Redux Toolkit/src/Components/From/From.jsx
+++ 04 User CRUD opration with Redux Toolkit/src/Components/From/From.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import { useState } from "react";
 
 function From() {
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+
+  const [user, setUser] = useState({});
+
+  const configureUser = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+
+  const handleSubmit = () => {};
+
   return (
     <>
-      <form className="w-50 mx-auto mt-5 h-full ">
+      <form className="w-50 mx-auto mt-5 h-full" onSubmit={handleSubmit}>
         <div className="mb-3">
           <label className="form-label">Email address</label>
           <input
@@ -11,6 +23,8 @@ function From() {
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            name="email"
+            onChange={configureUser}
           />
         </div>
         <div className="mb-3">
@@ -19,6 +33,18 @@ function From() {
             type="text"
             className="form-control"
             id="exampleInputPassword1"
+            name="username"
+            onChange={configureUser}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">age</label>
+          <input
+            type="number"
+            className="form-control"
+            id="exampleInputPassword1"
+            name="age"
+            onChange={configureUser}
           />
         </div>
         <div className="my-3">
@@ -26,19 +52,25 @@ function From() {
             <input
               className="form-check-input"
               type="radio"
-              name="flexRadioDefault"
+              // name="flexRadioDefault"
               id="flexRadioDefault1"
+              name="gender"
+              value="male"
+              onChange={configureUser}
             />
-            <label className="form-check-label">Default radio</label>
+            <label className="form-check-label">male</label>
           </div>
           <div className="form-check">
             <input
               className="form-check-input"
               type="radio"
-              name="flexRadioDefault"
+              // name="flexRadioDefault"
               id="flexRadioDefault2"
+              name="gender"
+              value="female"
+              onChange={configureUser}
             />
-            <label className="form-check-label">Default checked radio</label>
+            <label className="form-check-label">female</label>
           </div>
         </div>
         <button type="submit" className="btn btn-primary">
